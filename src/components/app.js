d8193f795c393e3e53da2a30f43ee5e0d711899e
Tristan Alexander Mouritsen <tmouritsen57@gmail.com>
2021-11-16 11:15:43 -0700

    initial commit

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Redis Random URL Generator</h1>
+        <h2>"For generating random URL's!"</h2>
+      </div>
+    );
+  }
+}
